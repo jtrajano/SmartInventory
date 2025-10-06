de9ff763ad20b2f1462c784677fc41158cd93554
=Jeff Trajano <=jlt212000@yahoo.com>
2025-10-06 13:19:24 +0800

    #2 Setup database context

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using SmartInventory.Application.Common.Interfaces;
+using SmartInventory.Domain.Entities;
+using SmartInventory.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartInventory.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<TodoList> TodoLists => Set<TodoList>();
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    public  DbSet<User> EntityUsers => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
