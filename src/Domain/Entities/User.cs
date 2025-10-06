739d51c3eefe1aff2efff8dc3edffd2ea0cd2726
=Jeff Trajano <=jlt212000@yahoo.com>
2025-10-06 13:19:41 +0800

    #3 Create User Entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Domain.Entities;
+public class User : BaseEntity
+{
+  
+    public string Email { get; private set; } = string.Empty;
+    public  string FullName { get; private set; } = string.Empty;
+    public  string Role { get; private set; } = string.Empty;
+    public string PasswordHash { get;private set; } = string.Empty;
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get;  set; } = DateTime.Now;
+
+    private User()
+    {
+        
+    }
+
+    public User(string email, string fullName, string role)
+    {
+        Email = email;
+        FullName = fullName;
+        Role = role;
+        IsActive = true;
+        CreatedAt = DateTime.Now;
+    }
+
+    public void Deactivate()=> IsActive = false;
+
+
+}
